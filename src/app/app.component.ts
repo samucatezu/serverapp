7cde409d0264fddf205eed245dada0ee0092a7da
Samuel Almeida <barbosasamuel24@gmail.com>
2022-06-06 19:08:45 -0300

    code refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,14 @@ import { Status } from './enum/status.enum';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { Server } from './interface/server';
-import { NotificationService } from './services/notification.service';
-import { ServerService } from './services/server.service';
+import { NotificationService } from './service/notification.service';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
   appState$: Observable<AppState<CustomResponse>>;
@@ -24,7 +25,8 @@ export class AppComponent implements OnInit {
   filterStatus$ = this.filterSubject.asObservable();
   private isLoading = new BehaviorSubject<boolean>(false);
   isLoading$ = this.isLoading.asObservable();
-  
+
+
   constructor(private serverService: ServerService, private notifier: NotificationService) { }
 
   ngOnInit(): void {
